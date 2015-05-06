2945bf8461ea3e87e10fad2708f4b98a8fce02a0
RoberMac <abc269513055@gmail.com>
2015-05-06 21:51:47 +0800

    bugfix-v0.3.7
    －優化：用戶進入／退出提醒

--- public/js/directives/sy-usercount.js
+++ public/js/directives/sy-usercount.js
@@ -2,15 +2,30 @@ angular.module('ShinyaApp.usercountDirective', [])
 .directive('syUsercount', function (){
     return {
         restrict: 'E',
-        template: '<div ng-if="!isMobile" id="userCount" ng-click="toggleAtUserBox()">'
+        template: '<div ng-if="!isMobile" id="userCount" class="faster_animate" ng-click="toggleAtUserBox()">'
                 +   '在線人數：{{userCount}}'
                 + '</div>',
-        controller: ['$scope', function ($scope){
+        controller: ['$scope', '$timeout', function ($scope, $timeout){
             
             // 在線人數
             $scope.userCount  = 0
             $scope.onlineUser = []
+            function userCountNotify(elem, type){
+                elem.classList.add(type)
+                $timeout(function (){
+                    elem.classList.remove(type)
+                }, 717)
+            }
+            // 有用戶加入／退出
             $scope.onUserIO = function(msg){
+                var elem = document.getElementById('userCount')
+                // 提醒
+                elem
+                ? $scope.userCount <= msg.count // 桌面端
+                    ? userCountNotify(elem, 'user_join')
+                    : userCountNotify(elem, 'user_left')
+                : null  // 移動端
+                // 更新
                 $scope.$apply(function (){
                     $scope.userCount  = msg.count
                     $scope.onlineUser = msg.onlineUser
